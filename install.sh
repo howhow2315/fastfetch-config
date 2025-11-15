a2017641ef12b0421567e0c114a4485af0745397
howhow <howhow112315@gmail.com>
2025-11-15 08:24:04 -0500

    modified:   README.md 	modified:   ascii/akigomi-dot.txt 	new file:   examples/example.png 	modified:   install.sh 	modified:   uninstall.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
+set -euo pipefail
 
-copymark="// howhow-fastfetch-config"
+CMD=$(basename "$0")
+
+# Display a message "[*] with a symbol prefix"
+_notif() {
+    local msg="${1:-""}" sym=${2:-"*"}
+    [[ -n "$msg" ]] && echo "[$sym] $msg"
+}
+
+# Display an error message and exit with a specified code (default 1)
+_err() {
+    local msg="$1" code=${2:-1}
+    _notif "$CMD ERROR: $msg" !
+    exit "$code"
+}
+
+# Run a command silently, suppressing stdout and stderr, shell-dependent
+case "$(basename "$SHELL")" in
+    bash|zsh) _silently() { "$@" &> /dev/null; } ;;
+    *) _silently() { "$@" > /dev/null 2>&1; } ;; # Default to POSIX-compatible syntax
+esac
+
+# Check if a command exists in PATH
+_hascmd() { _silently command -v "$1"; }
+
+
+# Installer
+
 repo="https://github.com/howhow2315/fastfetch-config"
-ff_conf_dir="$HOME/.local/share/fastfetch"
+ff_conf_dir="$HOME/.config/fastfetch"
 ff_conf="$ff_conf_dir/config.jsonc"
+ascii_dir="$ff_conf_dir/ascii"
+ascii_file="$ascii_dir/ascii.txt"
 
-mkdir -p "$ff_conf_dir" "$ff_conf_dir/ascii"
-curl -L "$repo/raw/main/ascii/akigomi-dot.txt" -o "$ff_conf_dir/ascii.txt"
+_hascmd fastfetch || _err "are you really installing a fastfetch config without fastfetch installed?"
+_hascmd curl || _err "'curl' is required."
 
-if [[ ! -f "$cfg" ]] || head -n1 "$cfg" == "$copymark" ; then
-    mkdir -p "$(dirname "$cfg")"
-    curl -L "$repo/raw/main/config.jsonc" -o "$cfg"
-    sed -i "1i$copymark" "$cfg"
-fi
+mkdir -p "$ff_conf_dir" "$ascii_dir"
+[[ ! -f "$ascii_file" ]] && _silently curl -fL "$repo/raw/main/ascii/akigomi-dot.txt" -o "$ascii_file"
+[[ ! -f "$ff_conf" ]] && _silently curl -fL "$repo/raw/main/config.jsonc" -o "$ff_conf"
+
+clear && fastfetch
--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
+set -euo pipefail
+
+# Run a command silently, suppressing stdout and stderr, shell-dependent
+case "$(basename "$SHELL")" in
+    bash|zsh) _silently() { "$@" &> /dev/null; } ;;
+    *) _silently() { "$@" > /dev/null 2>&1; } ;; # Default to POSIX-compatible syntax
+esac
+
+# Check if a command exists in PATH
+_hascmd() { _silently command -v "$1"; }
+
+
+# Uninstaller
 
 copymark="// howhow-fastfetch-config"
 repo="https://github.com/howhow2315/fastfetch-config"
-ff_conf_dir="$HOME/.local/share/fastfetch"
+ff_conf_dir="$HOME/.config/fastfetch"
 ff_conf="$ff_conf_dir/config.jsonc"
+ascii_dir="$ff_conf_dir/ascii"
+ascii_file="$ascii_dir/ascii.txt"
 
-mkdir -p "$ff_conf_dir" "$ff_conf_dir/ascii"
-curl -L "$repo/raw/main/ascii/akigomi-dot.txt" -o "$ff_conf_dir/ascii.txt"
-
-if [[ ! -f "$cfg" ]] || head -n1 "$cfg" == "$copymark" ; then
-    mkdir -p "$(dirname "$cfg")"
-    curl -L "$repo/raw/main/config.jsonc" -o "$cfg"
-    sed -i "1i$copymark" "$cfg"
-fi
+[[ -f "$ascii_file" ]] && rm "$ascii_file"
+[[ -z "$(ls -A "$ascii_dir")" ]] && rm -r "$ascii_dir"
+[[ -f "$ff_conf" ]] && [[ $(head -n 1 "$ff_conf") == "$copymark" ]] && rm "$ff_conf"
+[[ -z "$(ls -A "$ff_conf_dir")" ]] && rm -r "$ff_conf_dir"
+_hascmd fastfetch && clear && fastfetch
